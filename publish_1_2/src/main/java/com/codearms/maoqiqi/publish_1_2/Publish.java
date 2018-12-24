1ab803afe101db608e5d2ea6ff2135ad85465ff2
maoqiqi <fengqi.mao.march@gmail.com>
2018-12-24 17:53:53 +0800

    Upload the publish_1_2 lib to bintray and use it.

--- publish_1_2/src/main/java/com/codearms/maoqiqi/publish_1_2/Publish.java
+++ publish_1_2/src/main/java/com/codearms/maoqiqi/publish_1_2/Publish.java
@@ -3,6 +3,6 @@ package com.codearms.maoqiqi.publish_1_2;
 public class Publish {
 
     public static String getInfo() {
-        return Publish.class.getName() + "中的getInfo()方法.";
+        return Publish.class.getName() + "中的getInfo()方法...";
     }
 }
